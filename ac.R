6cae65d68f304e5161c5a7e9cb204acaa0d731f7
lilimatic <lilimaticsg@gmail.com>
2018-05-26 23:27:39 +0200

    Update ac.R

--- ac.R
+++ ac.R
@@ -1,10 +1,18 @@
-t=0
-X=numeric(0)
-while(X[length(X)]<=Tmax){
-  u=runif(1)
-  t=t-log(u)/lambdau
-  if(runif(1)<=lambda(t)/lambdau) 
+#za datu funkciju lambda i lambdafix
+ac = function(){
+  norm <- function() {
+    while (1) {
+      u = runif(1)
+      y = -log(u)
+      u1 = runif(1)
+      t= -log(u)/lambdafix
+      if (u <= lambda(t)/lambdafix) {
+        ifelse(runif(1) >= 0.5, return(y), return(-y))
+      }
+    }
     X=c(X,t)
+  }
+
 }
 
 
