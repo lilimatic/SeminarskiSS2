5d26a9b1af4699176b754ac20d05acd42f804539
lilimatic <lilimaticsg@gmail.com>
2018-05-26 23:16:26 +0200

    Update ac.R

--- ac.R
+++ ac.R
@@ -1,10 +1,10 @@
-####Njegov kod
 t=0
 X=numeric(0)
 while(X[length(X)]<=Tmax){
   u=runif(1)
   t=t-log(u)/lambdau
-  if(runif(1)<=lambda(t)/lambdau) X=c(X,t)
+  if(runif(1)<=lambda(t)/lambdau) 
+    X=c(X,t)
 }
 
 
