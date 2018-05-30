c32f93176272d3e9aea9860cb02042bfa184456d
lilimatic <lilimaticsg@gmail.com>
2018-05-30 12:06:55 +0200

    Update ac.R

--- ac.R
+++ ac.R
@@ -1,18 +1,19 @@
-#za datu funkciju lambda i lambdafix
+#za datu funkciju Lambda i lambdafix
 #za nasu optimizajucu konstantu koristimo fiksirano lambda iz homogenog Puasonovog procesa
 ac = function(){
     while (1) {
         #pravimo u
       u = runif(1)
-        #eksponencijalnu
-      y = -log(u)
+        #eksponencijalna
       u1 = runif(1)
-        #generisemo t metodom inverzne transformacije
+        #generisemo t metodom inverzne transformacija
       t= -log(u)/lambdafix
         #Algoritam za AC method
+        #Generisemo T
       if (u1 <= Lambda(t)/lambdafix) {
           #gledamo u koju polovinu je zapalo i po tome posledjujemo 
-        ifelse(runif(1) >= 0.5, return(y), return(-y))
+          #Pa se ili vracamo na korak 3, ili prihvatamo. 
+        ifelse(runif(1) >= 0.5, return(t), return(-t))
       }
   }
 
