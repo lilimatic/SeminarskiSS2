010ebf5021585f0410c5926ac0aa53cdeceb6443
lilimatic <lilimaticsg@gmail.com>
2018-05-30 13:14:35 +0200

    Update inverzna.R

--- inverzna.R
+++ inverzna.R
@@ -8,13 +8,17 @@ inverzna_transformacija = function(T_n,Lambda,n) {
   T = c(0)
   while(T[length(T)] < T_n) {
     U=runif(1)
+    #analogno kao i sa casa 
     s=s-log(U)
     #trazimo infimum td. \Lambda(a)=>s der po metodu 
     #F^{-1}(a)= inf{a:\Lambda(a)>s}
-    #Funkcija vectorize radi isto kao apply funkcija
-    t=min(a[which(vectorize(Lambda)(a)>=s)])
+    #Funkcija sapply racuna sve vrednosti od Lambda u zavisnosti od a
+    #t je minimalna svih vrednosti od a za koje je inf{a:\Lambda(a)>s}
+    #to ponavljamo sve dok ne izadjemo iz petlje
+    t=min(a[which(sapply(a,Lambda)>=s)])
     T=c(T,t)
   }
+  #vraca nasu inverznu transformaciju
   return(T)
 }
 
