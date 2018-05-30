0f101aec04c7dc6be0f5d8e827bc3f83bb00af72
lilimatic <lilimaticsg@gmail.com>
2018-05-30 11:17:12 +0200

    Update ac.R

--- ac.R
+++ ac.R
@@ -1,19 +1,15 @@
 #za datu funkciju lambda i lambdafix
 ac = function(){
-  norm <- function() {
     while (1) {
       u = runif(1)
       y = -log(u)
       u1 = runif(1)
       t= -log(u)/lambdafix
-      if (u <= lambda(t)/lambdafix) {
+      if (u1 <= lambda(t)/lambdafix) {
         ifelse(runif(1) >= 0.5, return(y), return(-y))
       }
-    }
     X=c(X,t)
   }
-
-}
 
 
 
